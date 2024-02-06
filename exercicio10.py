ded74e416abe26b3c0d3ac8ec1b65ac29cb95ac6
Lucas Oliveira <oliveiralucas813@gmail.com>
2024-02-05 22:26:07 -0300

    exercicios 05/2

---
+++ exercicio10.py
@@ -0,0 +1,5 @@
+real= float(input('Insira o valor que você tem em R$: '))
+
+conv= real /4.89
+
+print(f'O valor em dolar que você pode comprar é de:{conv:.2f}')
---
+++ exercicio11.py
@@ -0,0 +1,11 @@
+lar= float(input('Insira a largura da parede: '))
+alt= float(input('Insira a altura da parede: '))
+
+area= lar*alt
+print(f'A area da parede é de: {area:.1f}m')
+tinta= area/2
+
+#print (f'Serão necessárias {tinta:.1f} litros de tinta')
+print(f'A quantidade de tinta necessária para pintar a parede é de {tinta}l de tinta')
+
+
