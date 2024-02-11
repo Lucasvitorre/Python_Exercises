c14ec9fbeab46760e993adcb50f8147fff71ac1c
Lucas Oliveira <oliveiralucas813@gmail.com>
2024-02-11 15:06:49 -0300

    Update manipulando_string.py

--- manipulando_string.py
+++ manipulando_string.py
@@ -1,26 +1,66 @@
 #FRASE DE TESTE
 frase='Curso em Video Python'
 
-#FATIAMENTO
+###------------FATIAMENTO-------------------###
 #SOMENTE UMA LETRA
 frase[9]
+
 #intervalo entre o 9 e o 12
 frase[9:13]
+
 #tudo até o 12º indice 
 frase[:13]
+
 #tudo depois do 14º indice
 frase[14:]
+
 #pulando de dois em dois
 frase[9:13:2] 
+
 #pulando de três em três até o final da frase
 frase[9::3]
 
-#ANÁLISE
+###-------------ANALISE-------------###
 #comprimento da frase
 len(frase)
+
 #contar a quantidade de letras: ('a','r')
 frase.count('o')
-frase.count('o', 0,13) # fazendo fatiamento em conjunto com o count
+#faz fatiamento em conjunto com o count
+frase.count('o', 0,13)
 
 #Procurar conjunto de string dentro da variavel
 frase.find('deo')
+
+#Valor boolean se há ou não a palavra na variavel.
+'Curso' in frase
+
+###-------------TRANSFORMAÇÃO--------------###
+#Alteração de palavras
+frase.replace('Python','Android')
+
+#TODAS MAIUSCULAS
+frase.upper()
+
+#TODAS MINUSCULAS
+frase.lower()
+
+#Somente o primeiro caracter em maiusculo
+frase.capitalize()
+
+#Todas as palavras após espaço em maiusculo
+frase.title()
+
+#Retirar todos espaços de frase 
+frase.strip
+frase.rstrip #espaços a direita
+frase.lstrip #espaços a esquerda
+
+###-------------Divisão--------------###
+#Divide por espaços
+frase.split()
+
+###-------------Junção--------------###
+'-'.join(frase)
+
+
