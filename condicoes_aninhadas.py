7d7314e9c23b255a682f4eb66aecba553d9b3143
Lucas Oliveira <95634605+Lucasvitorre@users.noreply.github.com>
2024-04-22 14:52:59 -0300

    Create condicoes_aninhadas

---
+++ condicoes_aninhadas.py
@@ -0,0 +1,11 @@
+#condicoes_aninhadas
+
+#Você pode utilizar dentro de um IF quantos ELIF's você quiser, e o ELSE só pode utilizar uma vez.
+#Não pode utilizar um ELIF sem o IF
+
+carro= input(str("Para qual direção o seu carro vai? "))
+if carro.esquerda():
+
+elif carro.direita():
+
+else
