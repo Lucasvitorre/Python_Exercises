a3314a59d054940f81608d3c6f5ef4f6ee4887f9
Lucas Oliveira <95634605+Lucasvitorre@users.noreply.github.com>
2024-05-14 16:41:16 -0300

    Update exercicio51.py

--- exercicio51.py
+++ exercicio51.py
@@ -1,2 +1,14 @@
 #DESENVOLVA UM PROGRAMA QUE LEIA O PRIMEIRO TERMO E A RAZÃO DE UMA PA.
 #NO FINAL MOSTRE OS 10 PRIMEIROS TERMOS DESSA PROGRESSÃO.
+
+print('Gerador de Pogressão Aritmética')
+print('#'*15)
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print('{} -> '.format(termo), end='')
+    termo += razao
+    cont += 1
+
