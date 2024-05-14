169afd759df354af68851da5ff8fd71bfd2ba4d7
Lucas Oliveira <95634605+Lucasvitorre@users.noreply.github.com>
2024-05-14 14:26:13 -0300

    Create exercicio48.py

---
+++ exercicio48.py
@@ -0,0 +1,5 @@
+#Faça um programa que calcule a soma entre todos os números impares que são múltiplos de três e que se encontram no intervalo de 1 até 500.
+
+for contador in range (1,500):
+    if contador % 3 == 0:
+        print(contador)
