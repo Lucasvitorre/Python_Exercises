4e329ecf978811595866159d69206f1115b41a8a
Lucas Oliveira <95634605+Lucasvitorre@users.noreply.github.com>
2024-05-14 16:31:44 -0300

    Update exercicio50.py

--- exercicio50.py
+++ exercicio50.py
@@ -1,2 +1,8 @@
 #DESENVOLVA UM PROGRAMA QUE LEIA SEIS NÚMEROS INTEIROS E MOSTRE A SOMA APENAS DAQUELAS QUE FOREM PARES.
 #SE O VALOR DIGITADO FOR IMPAR, DESCONSIDERE-O.
+soma = 0
+for c in range(1, 7):
+    n = int(input('Digite o {}º valor: '.format(c)))
+    if n % 2 == 0:
+        soma += n
+        print('A soma dos números pares é {}'.format(soma))
