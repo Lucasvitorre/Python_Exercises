7bdbfcfad0bf5a5d6f918525f07273e32b50fba0
Lucas Oliveira <95634605+Lucasvitorre@users.noreply.github.com>
2024-05-14 16:22:14 -0300

    Update exercicio49.py

--- exercicio49.py
+++ exercicio49.py
@@ -1,1 +1,10 @@
 #REFAÇA O DESAFIO 009, MOSTRANDO A TABUADA DE UM NÚMERO QUE O USUÁRIO ESCOLHER, SÓ QUE AGORA UTILIZANDO UM LAÇO FOR.
+
+print("Programa Tabuada 2.0")
+
+num = int(input("Insira o valor: "))
+for tabuada in range (1, 10 + 1):
+    tab = tabuada * num
+    print(f"{num} x {tabuada} = {tab}")
+
+print("FIM")
