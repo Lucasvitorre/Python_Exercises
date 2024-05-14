b648ee81ace1bf9024a2751a461e735936c81063
Lucas Oliveira <95634605+Lucasvitorre@users.noreply.github.com>
2024-05-14 12:11:49 -0300

    Update exercicio47.py

--- exercicio47.py
+++ exercicio47.py
@@ -1,6 +1,10 @@
-import time
+from time import sleep
 import math
 
-lista= (0,50)
+#CRIE UM PROGRAMA QUE MOSTRE NA TELA "TODOS OS NÚMEROS PARES" QUE ESTÃO NO INTERVALO ENTRE 1 E 50.
 
-print (lista)
+for intervalo in range (1, 50):
+    if intervalo % 2 == 0:
+        sleep(1)
+        print(f"Os números pares são: {intervalo}")
+print (f"Esse são os números pares para o intervalo de 1 a 50!")
