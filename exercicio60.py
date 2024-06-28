8e5a495e17b563736ab8e8d3518e27f9c73168c1
Lucas Oliveira <95634605+Lucasvitorre@users.noreply.github.com>
2024-06-28 16:09:35 -0300

    Update exercicio60.py

--- exercicio60.py
+++ exercicio60.py
@@ -5,8 +5,3 @@ v = int(input("Informe o valor que deseja verificar: "))
 print("Aguarde:...")
 resultado = factorial(v)
 print(f"O Valor Fatorial do número informado é:{resultado} ")
-
-    
-    
-
-        
