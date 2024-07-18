80412181c14088b4b0c70defdbe1ba083599092b
Lucas Oliveira <oliveiralucas813@gmail.com>
2024-07-18 19:24:57 -0300

    atualização-arquivos

--- ANOTAÇOES/teste.py
+++
@@ -1,8 +0,0 @@
-from time import sleep
-
-numero = int(input("Digite um número: "))
-while numero < 100:
-    print(str(numero))
-    numero = numero + 1
-    sleep(0.5)
-print("Laço encerrado....")
