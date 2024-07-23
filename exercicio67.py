7ac1630225ca3b6e5460ab19bee13006e1fc49e0
Lucas Oliveira <95634605+Lucasvitorre@users.noreply.github.com>
2024-07-23 18:17:43 -0300

    Create exercicio67.py

---
+++ exercicio67.py
@@ -0,0 +1,13 @@
+#FAÇA UM PROGRAMA QUE MOSTRE A TABUADA DE VARIOS NÚMEROS, UM DE CADA VEZ, PARA CADA VALOR DIGITADO
+#O PROGRAMA SERÁ INTERROMPIDO QUANDO O NÚMERO SOLICITADO FOR NEGATIVO
+tabuada = 0
+resultado = tabuada * 1
+
+while True:
+    tabuada= int(input("Qual tabuada você quer saber? "))
+    print("-"*20)
+    print(f"{tabuada} x 1 = ")
+
+    if tabuada < 0:
+        break
+print("PROGRAMA TABUADA ENCERRADO. Volte Sempre!")
