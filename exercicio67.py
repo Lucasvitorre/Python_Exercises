e86367af96327ad2a7a4818cace38a199bff1774
Lucas Oliveira <95634605+Lucasvitorre@users.noreply.github.com>
2024-07-23 18:27:04 -0300

    Update exercicio67.py

--- exercicio67.py
+++ exercicio67.py
@@ -1,12 +1,13 @@
 #FAÇA UM PROGRAMA QUE MOSTRE A TABUADA DE VARIOS NÚMEROS, UM DE CADA VEZ, PARA CADA VALOR DIGITADO
 #O PROGRAMA SERÁ INTERROMPIDO QUANDO O NÚMERO SOLICITADO FOR NEGATIVO
 tabuada = 0
-resultado = tabuada * 1
 
 while True:
     tabuada= int(input("Qual tabuada você quer saber? "))
     print("-"*20)
-    for range
+    for count in range(10):
+        resultado = tabuada * count+1
+        print(f"{tabuada} x {count +1 } = {resultado}" )
 
     if tabuada < 0:
         break
