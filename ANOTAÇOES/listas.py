3ffb4b4200c7ef51a5586195f4183f9451cfde12
Lucas Oliveira <oliveiralucas813@gmail.com>
2024-08-20 20:26:46 -0300

    Update listas.py

--- ANOTAÇOES/listas.py
+++ ANOTAÇOES/listas.py
@@ -18,7 +18,19 @@ print(lanche)
 lanche.append("Cookie")
 print(lanche)
 print #####
+
 #Adicionando um objeto em determinadas posições:
 lanche.insert(0,"Churrasco")
 print(lanche)
+
+#Removendo um objeto, diversas formas
+
+del lanche[3]
+lanche.pop(3) or lanche.pop() #Geralmente utilzado para remover o ultimo elemento, mas é possivel passar um elemento especifico.
+lanche.remove("Churrasco") #Remover pelo conteúdo e náo pelo indice.
+print(lanche)
+
+if "Churrasco" in lanche:
+    lanche.remove("Churrasco")
+
 #DICIONARIOS SÃO REPRESENTADOS COM: {}
