11e66a81689bfc84ba5b89488512510894cb8828
Lucas Oliveira <95634605+Lucasvitorre@users.noreply.github.com>
2024-08-21 17:55:23 -0300

    Update funcoes.py

--- ANOTAÇOES/funcoes.py
+++ ANOTAÇOES/funcoes.py
@@ -32,13 +32,66 @@ contador(2,1,7)
 contador(8,0)
 contador(4,4,4,4,4,4,4)
 
-#EXEMPLO
+#EXEMPLO DE LISTA DENTRO DE FUNCAO
 def dobra(lst):
     pos =0
-    while pos<len(lst):
+    while pos < len(lst):
         lst[pos]*=2
         pos+=1
 
 valores = [7,2,5,0,4]
 dobra(valores)
 print(valores)
+
+#DESEMPACOTAMENTO
+def soma(* valores):
+    s = 0
+    for num in valores:
+        s += num
+    print(f"Somando os valores {valores} temos {s}")
+
+soma (8,9,10)
+
+#AULA 2 DE FUNCOES ##
+
+# INTERACTIVE HELP
+##
+##   BASTA SOMENTE UTILIZAR A FUNÇÃO HELP() no python
+help(print) #direto no código
+
+# DOCSTRINGS
+
+###    DOCSTRINGS É UMA DOCUMENTAÇÃO QUE UTILIZA """ """
+
+
+def contador (i,f,p):
+    """
+    --> FAZ uma contagem e mostra na tela.
+        :param i: inicio da contagem
+        :param f: fim da contagem
+        :param p: passo da contagem
+        :return: sem retorno
+    """
+    c = i
+    while c <= f:
+        print(f"{c}",end="")
+        c += p
+    print("Fim!")
+help(contador)
+contador(2,10,2)
+
+# PARAMETROS OPCIONAIS
+def somar(a,b,c=0): # Adicionar um parametro basta adicionar o operadorador dentro da funcao
+    s = a+b+c
+    print(f"A soma vale (S)")
+
+
+somar(3,2,5)
+
+# ESCOPO DE VARIAVEIS
+def teste():
+    print(f"Na função teste, n vale {n}")
+
+n = 2
+print(f"No programa princial, n vale {n}")
+# RETORNO DE RESULTADOS
