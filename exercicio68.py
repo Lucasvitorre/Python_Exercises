726acc75a8d044ae116ee19865da754537de858a
Lucas Oliveira <95634605+Lucasvitorre@users.noreply.github.com>
2024-08-21 11:54:00 -0300

    update

--- exercicio68.py
+++ exercicio68.py
@@ -1,10 +1,12 @@
 import random
-while "impar":
+while True:
+
     num1 = int(input("Digite o número: "))
     num2 = random.randint(0,20)
     soma = num1 + num2 
 
-
-    escolha = input("Par ou Impar? [P/I]: ")
-
-print(f"Você escolheu {num1} e o computador {num2}, o valor da soma é {soma}.")
+    escolha = input(str("Par ou Impar? [P/I]: "))
+    sair = input("Deseja continuar? ").upper()
+    print(sair)
+    if sair == "NAO":
+        break
